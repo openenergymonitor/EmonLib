a23a1d7ec6921c7fd925c3e9614e8a8e71b80ec1
Glyn Hudson <glyn.hudson@gmail.com>
2012-10-22 14:07:09 +0100

    correct missing #elif define for ATtiny85

--- EmonLib.cpp
+++ EmonLib.cpp
@@ -220,8 +220,9 @@ long EnergyMonitor::readVcc() {
   #elif defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
   ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
   #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-    ADMUX = _BV(MUX5) | _BV(MUX0);
+  ADMUX = _BV(MUX5) | _BV(MUX0);
   #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
   #endif
 
   delay(2);					// Wait for Vref to settle
