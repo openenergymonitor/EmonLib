347f616da7ef6588db32ab5f349ff6f5cc6cba46
Glyn Hudson <glyn.hudson@gmail.com>
2013-04-14 16:54:25 +0200

    extract all calculated values out into variables for easy access

--- examples/voltage_and_current/voltage_and_current.ino
+++ examples/voltage_and_current/voltage_and_current.ino
@@ -14,5 +14,11 @@ void setup()
 void loop()
 {
   emon1.calcVI(20,2000);         // Calculate all. No.of half wavelengths (crossings), time-out
-  emon1.serialprint();           // Print out all variables
+  emon1.serialprint();           // Print out all variables (realpower, apparent power, Vrms, Irms, power factor)
+  
+  float realPower       = emon1.realPower;        //extract Real Power into variable
+  float apparentPower   = emon1.apparentPower;    //extract Apparent Power into variable
+  float powerFActor     = emon1.powerFactor;      //extract Power Factor into Variable
+  float supplyVoltage   = emon1.Vrms;             //extract Vrms into Variable
+  float Irms            = emon1.Irms;             //extract Irms into Variable
 }
